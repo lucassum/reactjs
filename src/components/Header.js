bab9bcd1b45b2de46cc5416acc7d88491f31dbbb
Lucas Jórdan <lucasjordandt@gmail.com>
2021-07-03 02:43:35 -0300

    commit

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "../header.css";
+function Header () {
+    return (
+        <header className="header">
+            <div>
+                <h1 className="title">Teste</h1>
+            </div>
+        </header>
+    )
+}
+export default Header
---
+++ src/components/Main.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Button from "./Button";
+import "../main.css";
+
+function Main () {
+    return (
+        <main>
+            <div className="container"></div>
+            <div className="side-right">
+                <input type="text"></input>
+                <Button text="ss" />
+            </div>
+        </main>
+    )
+}
+
+export default Main
